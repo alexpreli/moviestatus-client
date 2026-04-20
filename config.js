951d6dbad295dc188d52b5fe6d5469fd38dab8d2
Alex Preli <tigerz0r7@gmail.com>
2026-04-20 23:21:45 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.MOVIESTATUS_CONFIG = {
-  region: "US",
-  tmdbLanguage: "en-US",
-  searchDailyLimit: 100,
-  searchVerifyBaseUrl: "localhost",
-  localServerPort: 3000,
-  searchVerifyDailyLimit: 100,
-  listingCheck: true,
-};
